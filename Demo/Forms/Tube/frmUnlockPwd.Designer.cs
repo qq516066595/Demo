d2985135c27e7a8381bc4a0a659a4964a0fbacd1
Smina <516066595@qq.com>
2020-12-29 10:18:25 +0800

    1

---
+++ Demo/Forms/Tube/frmUnlockPwd.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Demo.Forms.Tube
+{
+    partial class frmUnlockPwd
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmUnlockPwd));
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.pictureEdit1 = new DevExpress.XtraEditors.PictureEdit();
+            this.btnOK = new DevExpress.XtraEditors.SimpleButton();
+            this.btnCancel = new DevExpress.XtraEditors.SimpleButton();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.txtPwd = new DevExpress.XtraEditors.TextEdit();
+            this.picError = new DevExpress.XtraEditors.PictureEdit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtPwd.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picError.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Appearance.Font = new System.Drawing.Font("Tahoma", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelControl1.Appearance.ForeColor = System.Drawing.Color.Red;
+            this.labelControl1.Appearance.Options.UseFont = true;
+            this.labelControl1.Appearance.Options.UseForeColor = true;
+            this.labelControl1.Location = new System.Drawing.Point(105, 28);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(308, 50);
+            this.labelControl1.TabIndex = 0;
+            this.labelControl1.Text = "警告：解除联锁失去任何保护，\r\n           非专业人员请勿操作！";
+            // 
+            // pictureEdit1
+            // 
+            this.pictureEdit1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.pictureEdit1.EditValue = global::Demo.Properties.Resources.Danger;
+            this.pictureEdit1.Location = new System.Drawing.Point(12, 12);
+            this.pictureEdit1.Name = "pictureEdit1";
+            this.pictureEdit1.Properties.Appearance.BackColor = System.Drawing.Color.Transparent;
+            this.pictureEdit1.Properties.Appearance.Options.UseBackColor = true;
+            this.pictureEdit1.Properties.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
+            this.pictureEdit1.Properties.ShowCameraMenuItem = DevExpress.XtraEditors.Controls.CameraMenuItemVisibility.Auto;
+            this.pictureEdit1.Properties.SizeMode = DevExpress.XtraEditors.Controls.PictureSizeMode.Zoom;
+            this.pictureEdit1.Properties.ZoomAccelerationFactor = 1D;
+            this.pictureEdit1.Size = new System.Drawing.Size(87, 77);
+            this.pictureEdit1.TabIndex = 1;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(91, 183);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(110, 40);
+            this.btnOK.TabIndex = 2;
+            this.btnOK.Text = "确认";
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(234, 183);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(110, 40);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "取消";
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Appearance.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelControl2.Appearance.Options.UseFont = true;
+            this.labelControl2.Location = new System.Drawing.Point(54, 124);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(54, 22);
+            this.labelControl2.TabIndex = 3;
+            this.labelControl2.Text = "密码：";
+            // 
+            // txtPwd
+            // 
+            this.txtPwd.EditValue = "";
+            this.txtPwd.Location = new System.Drawing.Point(123, 121);
+            this.txtPwd.Name = "txtPwd";
+            this.txtPwd.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPwd.Properties.Appearance.Options.UseFont = true;
+            this.txtPwd.Properties.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.Simple;
+            this.txtPwd.Properties.NullValuePrompt = "请输入密码···";
+            this.txtPwd.Properties.NullValuePromptShowForEmptyValue = true;
+            this.txtPwd.Properties.PasswordChar = '*';
+            this.txtPwd.Properties.ShowNullValuePromptWhenFocused = true;
+            this.txtPwd.Size = new System.Drawing.Size(239, 32);
+            this.txtPwd.TabIndex = 4;
+            this.txtPwd.EditValueChanged += new System.EventHandler(this.txtPwd_EditValueChanged);
+            // 
+            // picError
+            // 
+            this.picError.Cursor = System.Windows.Forms.Cursors.Default;
+            this.picError.EditValue = ((object)(resources.GetObject("picError.EditValue")));
+            this.picError.Location = new System.Drawing.Point(368, 121);
+            this.picError.Name = "picError";
+            this.picError.Properties.Appearance.BackColor = System.Drawing.Color.Transparent;
+            this.picError.Properties.Appearance.Options.UseBackColor = true;
+            this.picError.Properties.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
+            this.picError.Properties.ShowCameraMenuItem = DevExpress.XtraEditors.Controls.CameraMenuItemVisibility.Auto;
+            this.picError.Properties.SizeMode = DevExpress.XtraEditors.Controls.PictureSizeMode.Zoom;
+            this.picError.Properties.ZoomAccelerationFactor = 1D;
+            this.picError.Size = new System.Drawing.Size(30, 32);
+            this.picError.TabIndex = 1;
+            this.picError.Visible = false;
+            // 
+            // frmUnlockPwd
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 250);
+            this.Controls.Add(this.txtPwd);
+            this.Controls.Add(this.labelControl2);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.picError);
+            this.Controls.Add(this.pictureEdit1);
+            this.Controls.Add(this.labelControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmUnlockPwd";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmUnlockPwd";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtPwd.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picError.Properties)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.PictureEdit pictureEdit1;
+        private DevExpress.XtraEditors.SimpleButton btnOK;
+        private DevExpress.XtraEditors.SimpleButton btnCancel;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private DevExpress.XtraEditors.TextEdit txtPwd;
+        private DevExpress.XtraEditors.PictureEdit picError;
+    }
+}
---
+++ Demo/Forms/Tube/frmUnlockPwd.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using com.DataBaseModels;
+/*
+* namespace：Demo.Forms.Tube
+* className：frmUnlockPwd
+* 
+* Version：HMI_RPxx_V001_202012
+* Author：hehf
+* Date：2020-12-28 18:21:59
+* Desc：解除联锁,目前为设置固定密码<123321>；如需设置可配置化则再完善
+* 
+* Remarks：权限控制待完善
+*/
+namespace Demo.Forms.Tube
+{
+    public partial class frmUnlockPwd : DevExpress.XtraEditors.XtraForm
+    {
+        public frmUnlockPwd()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// 确认
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            if(txtPwd.Text == "123321")
+            {
+                MyModus.gbBoatPush_Unlock = true;
+                this.Close();
+            }
+            else
+            {
+                txtPwd.Text = "";
+                picError.Visible = true;
+            }
+        }
+
+        private void txtPwd_EditValueChanged(object sender, EventArgs e)
+        {
+            picError.Visible = false;
+        }
+        /// <summary>
+        /// 取消
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
